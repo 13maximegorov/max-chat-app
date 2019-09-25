1b4ceae3daf3c7b8aebe93f5db097a194036b61e
Maxim Egorov <13maximegorov@gmail.com>
2019-09-25 09:47:42 +0500

    connect

--- plugins/socket.js
+++ plugins/socket.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
+const nuxt = new Nuxt(config)
+const { host, port } = nuxt.options.server
 
 export default function({store}) {
     Vue.use(new VueSocketIO({
         debug: false,
-        connection: 'http://localhost:3000',
+        connection: `http://${host}:${port}`,
         vuex: {
             store,
             actionPrefix: 'SOCKET_',
