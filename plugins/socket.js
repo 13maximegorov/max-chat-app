957b77c2d737d107fabbfd38d387a93349d941c0
Maxim Egorov <13maximegorov@gmail.com>
2019-09-25 10:42:09 +0500

    socket

--- plugins/socket.js
+++ plugins/socket.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import VueSocketIO from 'vue-socket.io'
-const {host, port} = require('../nuxt.config.js').server
+// import Vue from 'vue'
+// import VueSocketIO from 'vue-socket.io'
+// const {host, port} = require('../nuxt.config.js').server
 
-export default function({store}) {
-    Vue.use(new VueSocketIO({
-        debug: false,
-        connection: `https://max-chat-application.herokuapp.com:${port}`,
-        vuex: {
-            store,
-            actionPrefix: 'SOCKET_',
-            mutationPrefix: 'SOCKET_'
-        }
-    }))
-}
+// export default function({store}) {
+//     Vue.use(new VueSocketIO({
+//         debug: false,
+//         connection: `https://max-chat-application.herokuapp.com:${port}`,
+//         vuex: {
+//             store,
+//             actionPrefix: 'SOCKET_',
+//             mutationPrefix: 'SOCKET_'
+//         }
+//     }))
+// }
